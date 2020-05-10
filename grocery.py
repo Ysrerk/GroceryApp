7da58c147a916e8a187d633c287b3f87c6e82b5f
Yasar ERKAN <ysr.erk2019@gmail.com>
2020-05-10 02:40:36 -0400

    i added calculation of  bread discount

--- grocery.py
+++ grocery.py
@@ -1,6 +1,7 @@
 
 
 sepettutar=0
+sayac=0
 
 class Product():
     #stock = {"soup": 0.65, "bread": 0.8, "milk": 1.3, "apples": 0.10}
@@ -17,17 +18,32 @@ class Product():
     def addbasket(self,miktar):
         self.miktar=miktar
         sepet[self.prdname]=self.miktar
+        for key, value in sepet.items():#yeni
+            temp2.append((key, value), )#yeni
+
+
     def calculatebasket(self,miktar):
         self.miktar=miktar
         product=self.prdname
         sepettutar=float(stock[product])*int(self.miktar)
+
         basketamount.append(sepettutar)
-        return sepettutar
+        if product=="bread":
+            breadindex=(basketamount.index(sepettutar))#yeni
+            breadindexl.append(breadindex)
+            return sepettutar, breadindexl[0]
+        else:
+            breadindex=""
+            return sepettutar, breadindex
 
 
 stock = {}
 sepet={}
 basketamount=[]
+temp2=[]#yeni
+temp3=[]#yeni
+tempsoup=[]#yeni
+breadindexl=[]#yeni
 
 soup=Product("soup","tin")
 soup.updatecost(10)
@@ -45,17 +61,22 @@ milk.addproduct()
 apples=Product("apples","single")
 apples.updatecost(40)
 apples.addproduct()
-#print(stock)
-# x="bread"
-# print(stock[x]*3)
 
-# for key,value in stock.items():
-#     print(key,":",value)
 
 
 while True:
 
     urun=input("Please write  below which product do you want  buy:\n")
+    if urun in sepet.keys():
+        print("you can not add same product in the basket")
+        answer=input("we will clear your basket do you want to continue")
+        if answer=="e":
+            sepet.clear()
+            urun = input("Please write  below which product do you want  buy:\n")
+        else:
+            break
+
+
     miktar=int(input("Please write below how many do you want this item \n"))
     if urun==bread.prdname:
         bread.addbasket(miktar)
@@ -76,12 +97,31 @@ while True:
     print("Do you want add new product in the basket?\nif your answer is yes please write 'E' your answer is no please write 'H' ")
     print("Items in the your basket ",sepet)
     secim=input("Please wriye your prefer")
+    sayac+=1
     if secim==("h"or "H"):
+        temp3 = temp2[::-1]#yeni
+        temp2=[]#yeni
+        temp2=temp3[0:sayac]#yeni
+        #basketamount=basketamount[::-1]#
+
+        for i in temp2:#yeni
+            for j in i:#yeni
+                if j == "soup":#yeni
+                    tempsoup.append(i[1])#yeni
+                    if sum(tempsoup)>=2:#yeni
+                        if "bread" in sepet.keys():#yeni
+                            basketamount[breadindexl[0]] = basketamount[breadindexl[0]] / 2#yeni
+
 
         print("#####################################")
         print("Items in the your basket :", sepet)
         print("#####################################")
         print("Total Amount: ",sum(basketamount))
+
+        # print(temp2)###yeni
+        # print(temp3)
+        # print(sayac)#yeni
+        # print(basketamount)
         break
 
 
